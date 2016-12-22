c22bf10e9d7622e744ac2554a5eac24f2119f419
jie.lin <dev.linjie@gmail.com>
2016-12-22 21:24:43 +0800

    add tcp server code

--- pyscript/client.py
+++ pyscript/client.py
@@ -1,10 +1,9 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import
+import socket
 import logging
 
 logger = logging.getLogger(__name__)
-
-import socket
 
 
 def simple_tcp_client(host='developers.google.cn', port=80, bytes_num=4096):
@@ -22,8 +21,7 @@ def simple_udp_client(host='developers.google.cn', port=80, bytes_num=4096):
 
 
 if __name__ == '__main__':
-    # response = simple_tcp_client()
-    # print 'test simple tcp client :', response
-    data, addr = simple_udp_client('127.0.0.1', port=10000)
-    print 'test simple udp client :', data, addr
-
+    response = simple_tcp_client(host='0.0.0.0', port=9999)
+    print 'test simple tcp client :', response
+    # data, addr = simple_udp_client('127.0.0.1', port=10000)
+    # print 'test simple udp client :', data, addr
--- pyscript/server.py
+++ pyscript/server.py
@@ -1,11 +1,35 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import
+import threading
+import socket
+import sys
 import logging
 
 logger = logging.getLogger(__name__)
 
-import socket
-import sys
+
+def simple_tcp_server(bind_ip="0.0.0.0", bind_port=9999):
+    # multi-threaded tcp server
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((bind_ip, bind_port))
+    server.listen(5)
+    print "[*] Listening on %s:%d" % (bind_ip, bind_port)
+
+    # this is our client-handling thread
+    def handle_client(client_socket):
+        # print out what the client sends
+        request = client_socket.recv(1024)
+        print "[*] Received: %s" % request
+        # send back a packet
+        client_socket.send("ACK!")
+        client_socket.close()
+
+    while True:
+        client, addr = server.accept()
+        print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
+        # spin up our client thread to handle incoming data
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler.start()
 
 
 def simple_udp_server():
@@ -24,5 +48,7 @@ def simple_udp_server():
             sent = sock.sendto(data, address)
             print >> sys.stderr, 'sent %s bytes back to %s' % (sent, address)
 
+
 if __name__ == '__main__':
-    simple_udp_server()
+    # simple_udp_server()
+    simple_tcp_server()
